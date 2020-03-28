39adbe660165d811503909767e3b175c3d75d6b1
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-03-27 23:58:02 -0700

    added link to api and html routes

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require(`express`);
+const logger = require(`morgan`);
+const mongoose = require(`mongoose`);
+
+const apiRoutes = require(`./routes/apiRoutes`);
+const htmlRoutes = require(`./routes/htmlRoutes`);
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger(`dev`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`public`));
+
+apiRoutes(app);
+htmlRoutes(app);
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
